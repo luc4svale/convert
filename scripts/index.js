6a65d65535b9350b608586a0b4efafc38256eedd
= <alucasvalle@gmail.com>
2024-12-01 13:17:20 -0300

    Adcionando cotações e obtendo elementos do formulário

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+//Cotação de moedas do dia
+const USD = 5.97
+const EUR = 6.34
+const GBP = 7.60
+
+
+//Obtendo elementos do formulário
+const form = document.querySelector("form");
+const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
