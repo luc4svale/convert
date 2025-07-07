d47902a2b5211f6ea55dc8d94714fc03a72096f5
Lucas Vale <alucasvalle@gmail.com>
2025-07-06 22:05:06 -0300

    feat(submitFormEvent): validate amount and currency values on submit form event
    On submit form event, ensures that amount to convert is a number and the currency is a valid option between USD, EUR and GBP

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const form = document.querySelector('form')
 const amountToConvert = form.querySelector('#amount')
+const currency = form.querySelector('#currency')
 
 amountToConvert.oninput = () => {
   const notDigitsRegex = /\D+/g
   amountToConvert.value = amountToConvert.value.replace(notDigitsRegex, '')
+}
+
+form.onsubmit = async (event) => {
+  event.preventDefault()
+
+  if (isNaN(amountToConvert.value)) {
+    return alert('Não foi possível realizar a conversão. Por favor, informe um valor válido.')
+  }
+
+  const validCurrencies = ['USD', 'EUR', 'GBP']
+  if (!validCurrencies.includes(currency.value)) {
+    return alert('Não foi possível realizar a conversão. Por favor, escolha uma moeda válida.')
+  }
 }
